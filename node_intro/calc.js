26ee6b7f4f51d79766d530d5cf37da02f79cd9bb
Daniel <daniel.paschal@learningfuze.com>
2017-01-08 19:24:47 -0800

    added node.js intro example

---
+++ node_intro/calc.js
@@ -0,0 +1,20 @@
+//expected input examples:
+  //2 + 4
+  //9 / 3
+  //8 * 2
+  //4 - 2
+//determine if you have enough arguments passed in to perform the proper operation.  Each item with a space around it will be considered an argument
+
+//determine if you have an operand amongst the arguments
+
+//call the math function
+
+//define the math function
+  //determine which mathematical operation to perform
+
+  //perform the math
+
+  //console.log the result
+  //return the result
+
+
