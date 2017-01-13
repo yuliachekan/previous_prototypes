ad13b73b8cefe313f5c225f9909059d93d58c5da
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2017-01-13 09:52:48 -0800

    Added ang routes

---
+++ ang_routes/script.js
@@ -0,0 +1,22 @@
+// Create the route module and name it routeApp
+
+// Config the routes
+
+         // route for the home page
+
+        // route for the about page
+
+        // route for the contact page
+
+
+// Create the controllers for the different pages below
+
+// home page controller
+    // Create a message to display in the view
+
+
+// about page controller
+    // Create a message to display in the view
+
+// contact page controller
+    // Create a message to display in the view
