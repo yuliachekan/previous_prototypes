c749683cc27f499796cfc276a68af6cd5d41ab62
Daniel Paschal <daniel.paschal@learningfuze.com>
2017-02-05 14:51:11 -0800

    added remaining php modules

---
+++ php_file_upload/uploader_ajax.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+</head>
+<body>
+	<form id="file_upload">
+		
+	</form>
+</body>
+</html>
---
+++ php_mailer/mail_handler.php
@@ -0,0 +1,46 @@
+<?php
+require_once('emailconfig.php');
+require('phpmailer/PHPMailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+
+
+$mail->Username = EMAIL_USER;                 // SMTP username
+$mail->Password = EMAIL_PASS;                 // SMTP password
+$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 587;                                    // TCP port to connect to
+$options = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
+$mail->smtpConnect($options);
+$mail->From = 'example@gmail.com';//your email sending account
+$mail->FromName = 'example name';//your email sending account name
+$mail->addAddress(/*your email address, or the email the sender if you are sending confirmation*/, /*email address user name*/);     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+$mail->addReplyTo(/*email address of the person sending the message, so you can reply*/);
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Here is the subject';
+$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+if(!$mail->send()) {
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message has been sent';
+}
+?>
