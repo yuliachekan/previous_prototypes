d59328cea292676fcd7bb77d8fae62ef1fb2da1d
Daniel Paschal <daniel.paschal@learningfuze.com>
2017-06-10 20:45:22 -0700

    added js file contents

--- node_file_upload/file_handler.js
+++ node_file_upload/file_handler.js
@@ -1,1 +1,12 @@
-<!-- File server -->
+const express = require('express');
+const app = express();
+
+
+app.post('/fileUpload', function (req, res) {
+console.log('****************handler for your upload******************************');
+
+}) 
+
+app.listen(3000, function () {
+  console.log('uploader receiver listening on port 3000!')
+})
