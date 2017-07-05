e31a801377f52cebac60c1c7aab8f09cdffeeedd
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-07-05 09:24:07 -0700

    Added node_mysql

---
+++ node_mysql/getData.js
@@ -0,0 +1,14 @@
+//require your modules: express, and mysql
+
+//define your connection info for mysql
+
+//define your handler for your mysql connection callback
+	//perform your mysql query call
+
+//define your handler for your mysql query call response
+	//handle your data from your response
+	//return the data in json format to the front end request
+
+//define your handler for the express web request
+
+//set up your express server and start listening
