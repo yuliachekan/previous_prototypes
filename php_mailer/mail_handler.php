d3f41dde1ba473199ea1738c9eaf65298dd00118
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-08-17 16:27:14 -0700

    Clarified php_mailer comments

--- php_mailer/mail_handler.php
+++ php_mailer/mail_handler.php
@@ -1,18 +1,18 @@
 <?php
-require_once('emailconfig.php');
+require_once('email_config.php');
 require('phpmailer/PHPMailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
-$mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail->SMTPDebug = 3;           // Enable verbose debug output. Change to 0 to disable debugging output.
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->isSMTP();                // Set mailer to use SMTP.
+$mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers.
+$mail->SMTPAuth = true;         // Enable SMTP authentication
 
 
-$mail->Username = EMAIL_USER;                 // SMTP username
-$mail->Password = EMAIL_PASS;                 // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+$mail->Username = EMAIL_USER;   // SMTP username
+$mail->Password = EMAIL_PASS;   // SMTP password
+$mail->SMTPSecure = 'tls';      // Enable TLS encryption, `ssl` also accepted, but TLS is a newer more-secure encryption
+$mail->Port = 587;              // TCP port to connect to
 $options = array(
     'ssl' => array(
         'verify_peer' => false,
@@ -21,11 +21,11 @@ $options = array(
     )
 );
 $mail->smtpConnect($options);
-$mail->From = 'example@gmail.com';//your email sending account
-$mail->FromName = 'example name';//your email sending account name
-$mail->addAddress(/*your email address, or the email the sender if you are sending confirmation*/, /*email address user name*/);     // Add a recipient
-//$mail->addAddress('ellen@example.com');               // Name is optional
-$mail->addReplyTo(/*email address of the person sending the message, so you can reply*/);
+$mail->From = 'example@gmail.com';  // sender's email address (shows in "From" field)
+$mail->FromName = 'Example Name';   // sender's name (shows in "From" field)
+$mail->addAddress('recipient1@example.com', 'First Recipient');  // Add a recipient
+//$mail->addAddress('ellen@example.com');                        // Name is optional
+$mail->addReplyTo('example@gmail.com');                          // Add a reply-to address
 //$mail->addCC('cc@example.com');
 //$mail->addBCC('bcc@example.com');
 
